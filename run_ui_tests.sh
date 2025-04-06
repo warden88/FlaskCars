2ec83e35e6590b54f8fe1e1b50439b57cd0d38f4
Ivan Hrytsyna <gis20072017@gmail.com>
2025-04-06 05:22:41 +0300

    fix: gitlab-ci

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-
+# Запуск Flask-приложения в фоновом режиме
 python app.py &
 
+# Проверяем, что сервер Flask готов к работе (ожидание доступности порта 5000)
+while ! nc -z localhost 5000; do   
+  sleep 1
+done
 
-sleep 5
-
-
+# Создаем директорию для хранения результатов
 mkdir -p tests/results
 
-
+# Запуск тестов с сохранением результатов в файл
 pytest -s -v tests/test_list_cars_ui.py > tests/results/ui_results.txt
